f601d09feb2fa963c90d05e5b598879ceae769f7
ivan andres <andresgom11012@gmail.com>
2023-07-09 16:10:44 -0500

    adds exercise number three

---
+++ exercices/02-mouse-follower/src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function FollowMouse () {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('efecto')
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({ x: 0, y: 0 })
+    }
+  }, [enabled])
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'}
+      </button>
+    </>
+  )
+}
+
+function App () {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
